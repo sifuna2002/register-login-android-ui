4340a31fc2ffcf609710841fbdf649f861f3a76d
sifuna2002 <sifunadone4704@gmail.com>
2022-09-02 15:36:33 +0300

    registration module

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -39,7 +39,7 @@ export function Text(props: TextProps) {
 
 export function View(props: ViewProps) {
   const { style, lightColor, darkColor, ...otherProps } = props;
-  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+  const backgroundColor = useThemeColor({ light: '#1f1a30', dark: darkColor }, 'background');
 
   return <DefaultView style={[{ backgroundColor }, style]} {...otherProps} />;
 }
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,5 +1,6 @@
-import { StyleSheet } from 'react-native';
 
+import { Button, Pressable, StyleSheet, TextInput } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps } from '../types';
@@ -7,9 +8,47 @@ import { RootTabScreenProps } from '../types';
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
+      <Icon name="arrow-left" size={20} color="#f0fff0" />
+      <Text style={styles.title}>Create Account</Text>
+      <Text style={styles.description}>please fill the input below here</Text>
+      <View style={styles.inputContainer}>
+        <View style={styles.textContainer}>
+          <Icon name="user" style={styles.icon} color='white' />
+          <View style={styles.contain}>
+            <Text style={styles.label}>Full Name</Text>
+            <TextInput style={styles.textInput}>Jhone Williams</TextInput>
+          </View>
+        </View>
+        <View style={styles.textContainer2}>
+          <Icon name="smartphone" style={styles.icon} color='white' />
+          <View style={styles.contain}>
+            <Text style={styles.label}>Phone</Text>
+          </View>
+        </View>
+        <View style={styles.textContainer2}>
+          <Icon name="mail" style={styles.icon} color='white' />
+          <View style={styles.contain}>
+            <Text style={styles.label}>Email</Text>
+          </View>
+        </View>
+        <View style={styles.textContainer2}>
+          <Icon name="lock" style={styles.icon} color='white' />
+          <View style={styles.contain}>
+            <Text style={styles.label}>Password</Text>
+          </View>
+        </View>
+        <View style={styles.textContainer2}>
+          <Icon name="lock" style={styles.icon} color='white' />
+          <View style={styles.contain}>
+            <Text style={styles.label}>Confirm Password</Text>
+          </View>
+        </View>
+        <View style={styles.contain2}>
+          <Pressable style={styles.button} >
+            <Text style={styles.buttonText}>SIGN UP</Text>
+          </Pressable>
+        </View>
+      </View>
     </View>
   );
 }
@@ -17,16 +56,70 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'flex-start',
+  },
+  buttonText: {
+  },
+  button:{
+    width:'60%',
+    borderRadius: 20,
+    height:40,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#0df6e3',
   },
   title: {
-    fontSize: 20,
+    fontSize: 35,
     fontWeight: 'bold',
+    color: '#fff',
   },
-  separator: {
+  description: {
+    fontSize: 20,
+    color: '#fff',
+  },
+  inputContainer: {
     marginVertical: 30,
-    height: 1,
-    width: '80%',
+    width: '100%',
   },
+  textContainer: {
+    height: 50,
+    backgroundColor:'#39304d',
+    flexDirection: 'row',
+    borderRadius: 6,
+    alignItems: 'center',
+    },
+    textContainer2: {
+      marginTop:15,
+      height: 40,
+      backgroundColor:'transparent',
+      flexDirection: 'row',
+      borderRadius: 6,
+      alignItems: 'center',
+      },
+    label: {
+      fontSize: 10,
+      fontWeight: 'bold',
+      color: '#fff',
+      },
+      textInput: {
+        fontSize: 10,
+        color: '#fff',
+        },
+      contain:{
+        marginLeft: 10,
+        backgroundColor:'transparent',
+        flexDirection: 'column',
+      },
+      contain2:{
+        marginTop: 10,
+        backgroundColor:'transparent',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      icon:{
+        fontSize: 25,
+        marginLeft: 15,
+      }
 });
